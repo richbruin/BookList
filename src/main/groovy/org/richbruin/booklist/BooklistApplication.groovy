ec15358f2a349f0669b0d45d14c7c5babc14ffab
Rich Bruin <bruinr@oclc.org>
2019-01-02 20:49:17 +0000

    Initial commit with a working but empty gradle build of a spring boot app

---
+++ src/main/groovy/org/richbruin/booklist/BooklistApplication.groovy
@@ -0,0 +1,14 @@
+package org.richbruin.booklist
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BooklistApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BooklistApplication, args)
+	}
+
+}
+
---
+++ src/test/groovy/org/richbruin/booklist/BooklistApplicationTests.groovy
@@ -0,0 +1,17 @@
+package org.richbruin.booklist
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class BooklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
